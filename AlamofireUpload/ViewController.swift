31006c52070eeeb3f8fa8f1cbc446d4385ba438d
manojkulkarni <mnjpkulkarni@gmail.com>
2018-01-05 18:40:00 -0500

    Initial commit

--- AlamofireUpload/ViewController.swift
+++ AlamofireUpload/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
@@ -19,7 +20,51 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        uploadImage(url: "https://orangevalleycaa.org/api/upload.php")
+    }
 
+    func uploadImage(url: String){
+        
+        //----***** Simple image upload *****------
+//        if let img = UIImage.init(named: "nyc_by_me.JPG"){
+//            if let data = UIImageJPEGRepresentation(img, 0.5){
+//                Alamofire.upload(data, to: url).uploadProgress(closure: { (progress) in
+//                    print("\(progress.fractionCompleted)")
+//                }).responseJSON(completionHandler: { (response) in
+//                    debugPrint(response)
+//                })
+//            }
+//        }
+        
+        
+        //----***** Multipart POST form upload *****-----
+        
+        if let fileUrl = Bundle.main.url(forResource: "nyc_by_me", withExtension: "JPG") {
+            Alamofire.upload(multipartFormData: { (multipartFormData) in
+                multipartFormData.append("Manoj".data(using: .utf8)!, withName: "username")
+                multipartFormData.append(fileUrl, withName: "fileBeingUploaded")
+            }, to: url, encodingCompletion: { (encodingResult) in
+                switch encodingResult {
+                case .failure(let error):
+                    print(error)
+                case .success(let req, let streaming, let fileUrl):
+                        req.responseJSON(completionHandler: { (response) in
+                            debugPrint(response)
+                        })
+                        req.uploadProgress(closure: { (progress) in
+                            print("\(progress.fractionCompleted)")
+                        })
+                }
+            })
+        }
+        
+        
+        
+        
+    }
 
 }
 
